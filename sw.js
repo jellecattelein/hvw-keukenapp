2489bdff9c36376c052704bfa32fd64b44cfd25b
jellecattelein <jelle@hvw.be>
2026-08-19 08:20:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    sw.js — HVW Keukenapp Service Worker
    ══════════════════════════════════════════ */
 
-const CACHE_NAME = 'hvw-keuken-v17';
+const CACHE_NAME = 'hvw-keuken-v29';
 const ASSETS = [
   '/hvw-keukenapp/',
   '/hvw-keukenapp/index.html',
